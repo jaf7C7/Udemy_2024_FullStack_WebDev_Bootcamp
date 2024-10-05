00b214c70ce3c5b1e71e41a751a458f92268e7c2
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-10-05 18:53:49 +0100

    Add conditionals.js

---
+++ 10.Javascript_101/conditionals.js
@@ -0,0 +1,13 @@
+/* Conditional Statements */
+
+console.log('Conditional Statements:')
+
+const value = 10;
+
+if (value == 10) {
+	console.log('value is 10');
+else if (value == 20) {
+	console.log('value is 20');
+else {
+	console.log('Something went wrong!');
+}
