85e1b6b165f6cc3f713325e8f662a4daf8e2a787
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-10-05 21:25:20 +0100

    Add string methods notes

---
+++ 10.Javascript_101/stringMethods.js
@@ -0,0 +1,14 @@
+/* String Methods */
+
+
+const str = ' abcdefg';
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+str.search(' ');  // 0
+str.slice(1, 4);  // 'abc'
+str.substr(1, 4);  // 'abcd'
+str.replace('abc', 'XXX');  // ' XXXdefg'
+str.toUpperCase();  // ' ABCDEFG'
+str.toLowerCase();  // ' abcdefg'
+str.trim();  // 'abcdefg'
+str.split();  // [' ', 'a', 'b', ..., 'g']
