d8f7e7cffd6b3b16625e3801a449bd1c0c1a36ab
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2024-10-08 21:42:56 +0100

    Add notes on loading and unloading events

--- 11.JavaScript_201/script.js
+++ 11.JavaScript_201/script.js
@@ -1,8 +1,23 @@
+/* Page Loading and Unloading Events */
+
+
+/*
+ * You may need to run a piece of code when the user leaves your webpage, for
+ * example to alert them about unsaved changes, or save form data so users can
+ * continue where they left off later on.
+ *
+ * You might also need to wait until the page has fully loaded before running
+ * some code (e.g. to select and manipulate HTML elements).
+ *
+ * To achieve these aims you can add an event listener to the window object.
+ */
+
 // Do nothing until the page has fully loaded.	This is good practice,
 // especially if the page has a lot of or deeply nested html.
-// window.onload = () => {
-//	alert('loaded');
-// }
+window.addEventListener('onload', () => {
+	alert('loaded');
+});
+
 
 // After the user has clicked, but before the page is refreshed.
 //
@@ -10,23 +25,16 @@
 // interaction with the site.	 "Without ANY interaction (even one click
 // anywhere) beforeunload won't be fired."
 // - https://stackoverflow.com/a/58064395
-
+//
+// See also:
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/beforeunload_event
-const beforeUnloadHandler = (event) => {
-	// Recommended
+window.addEventListener("beforeunload", (event) => {
+	console.log(document.querySelector("#name").value)
 	event.preventDefault();
-
-	// Included for legacy support, e.g. Chrome/Edge < 119
-	event.returnValue = true;
-};
-
-const nameInput = document.querySelector("#name");
-
-nameInput.addEventListener("input", (event) => {
-	if (event.target.value !== "") {
-		window.addEventListener("beforeunload", beforeUnloadHandler);
-	} else {
-		window.removeEventListener("beforeunload", beforeUnloadHandler);
-	}
 });
 
+// 'beforeunload' can be handled inconsistently (e.g. in Firefox), so you
+// can also use the 'pagehide' event.
+window.addEventListener("pagehide", () => {
+	console.log(document.querySelector("#name").value);
+});
